1b124c9dce42df6751d4db206c46573a072068e1
Jank1510 <jeanhurt15@gmail.com>
2023-01-19 10:36:37 -0500

    update

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Karla', 'Lara')
+];
+
+function mostrarPersonas() {
+    console.log('Mostrar personas...');
+    let texto = '';
+    for (let persona of personas) {
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona() {
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if (nombre.value != '' && apellido.value != '') {
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+        nombre.value = ''
+        apellido.value = ''
+    }
+    else {
+        console.log('No hay información que agregar');
+        if (nombre.value == '') {
+            nombre.focus()
+        }else if(apellido.value==''){
+            apellido.focus()
+        }
+    }
+}
